4d6ab2c5a83ccca3f227707f15bab15638cf6809
Lekhacquocbao <quocbao153.iridi77@gmail.com>
2024-04-02 16:27:40 +0700

    02042024c

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,8 @@
 import db from "../models/index";
 import bcrypt from 'bcryptjs';
 
+const salt = bcrypt.genSaltSync(10);
+
 let handleUserLogin = (email, password) => {
     return new Promise(async(resolve, reject) => {
         try {
@@ -84,7 +86,110 @@ let getAllUsers = (userId) => {
     })
 }
 
+let hashUserPassword = (password) => {
+    return new Promise(async(resolve, reject) => {
+        try {
+            let hashPassword = await bcrypt.hashSync(password, salt); 
+            // let hashPassWord = await bcrypt.hashSync("B4c0/\/", salt);
+            resolve(hashPassword); 
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
+let createNewUser = (data) => {
+    return new Promise(async(resolve, reject) => {
+        try {
+            let check = await checkUserEmail(data.email);
+            if (check === true) {
+                resolve({
+                    errCode: 1,
+                    message: 'Email cua may da duoc su dung, nhap email khac di'
+                })
+            }
+            let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+            await db.User.create({
+                email: data.email,
+                password: hashPasswordFromBcrypt,
+                firstName: data.firstName,
+                lastName: data.lastName,
+                address: data.address,
+                phoneNumber: data.phoneNumber,
+                gender: data.gender === '1' ? true : false,
+                roleId: data.roleId,
+            })
+            resolve({
+                errCode: 0,
+                message: "Oke hehe"
+            })
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
+
+let deleteUser = (userId) => {
+    return new Promise(async(resolve, reject) => {
+        let user = await db.User.findOne({
+            where : {id :userId}
+        })
+        if (!user) {
+            resolve({
+                errCode: 2,
+                errMessage: `Tai khoan khong ton tai`
+            })
+        }
+        await db.User.destroy({
+            where: { id : userId}
+        });
+        resolve({
+            errCode: 0,
+            errMessage: `Tai khoan da duoc xoa nha`
+        })
+    })
+}
+
+let updateUserData = (data) => {
+    return new Promise(async(resolve, reject) => {
+        try {
+            if (!data.id) {
+                resolve({
+                    errCode: 2,
+                    errMessage: 'Missing required parameters'
+                })
+            }
+                let user = await db.User.findOne({
+                    where: {id: data.id},
+                    raw : false
+                })
+                if (user) {
+                        user.firstName = data.firstName;
+                        user.lastName = data.firstName;
+                        user.address = data.address;
+
+                    await db.User.save();
+                    
+                    resolve({
+                        errCode: 0,
+                        message: "Da update thanh cong"
+                    });
+                } else {
+                    resolve({
+                        errCode: 1,
+                        message: "Khong tim thay user"
+                    });
+                }
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
 module.exports = {
     handleUserLogin : handleUserLogin,
-    getAllUsers : getAllUsers
+    getAllUsers : getAllUsers,
+    createNewUser : createNewUser,
+    deleteUser : deleteUser,
+    updateUserData : updateUserData
 }
